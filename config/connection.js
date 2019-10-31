c93ce021d23cac3a9835378a2a5691aacce7a807
William Gonzales <wgonz94@yahoo.com>
2019-10-31 13:45:15 -0700

    configured config, controllers, and models

--- config/connection.js
+++ config/connection.js
@@ -1,1 +1,22 @@
 // Connect to MySQL
+var mysql = require("mysql");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "Redfaltor1!",
+  database: "burgers_db"
+});
+
+// Make connection.
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
+
+// Export connection for ORM to use.
+module.exports = connection;
--- models/burger.js
+++ models/burger.js
@@ -1,8 +1,38 @@
-var orm = require("orm.js");
+var orm = require("../config/orm.js");
 
-//create code to call ORM functions using burger specific input for the ORM
+//create code to call ORM functions using burger specific input
+//CRUD
 
+var burger = {
+    all: function(cb) {
+        orm.all("burgers", function(res) {
+            cb(res);
+        });
+    },
 
+    //columns and values
+    create: function(cols, vals, cb) {
+        orm.create("burgers", cols, vals, function(res) {
+            cb(res);
+        });
+    },
+
+    update: function(objColvals, condition, cb) {
+        orm.update("cats", objColVals, condition, function(res) {
+            cb(res);
+        });
+    },
+
+    delete: function(condition, cb) {
+        orm.delete("cats", condition, function(res) {
+            cb(res);
+        });
+    }
+
+};
+
+//export database functions to controller
+module.exports = burger;
 
 
 
