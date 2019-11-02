e89c4b4c31f1820690a15c71c257b73d3ea2e0d2
William Gonzales <wgonz94@yahoo.com>
2019-11-02 11:43:58 -0700

    added gitignore for login info for mysql

--- db/schema.sql
+++ db/schema.sql
@@ -1,12 +1,12 @@
-DROP DATABASE IF EXISTS burgers_db;
+-- DROP DATABASE IF EXISTS burgers_db;
 
-CREATE DATABASE burgers_db;
+-- CREATE DATABASE burgers_db;
 
-USE burgers_db;
+-- USE burgers_db;
 
-CREATE TABLE burgers (
-    id INTEGER AUTO_INCREMENT NOT NULL,
-    burger_name VARCHAR(50) NOT NULL,
-    devoured BOOLEAN DEFAULT false,
-    PRIMARY KEY (id)
-)
+-- CREATE TABLE burgers (
+--     id INTEGER AUTO_INCREMENT NOT NULL,
+--     burger_name VARCHAR(50) NOT NULL,
+--     devoured BOOLEAN DEFAULT false,
+--     PRIMARY KEY (id)
+-- )
